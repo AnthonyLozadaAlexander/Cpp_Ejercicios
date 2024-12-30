9fac95ff7bf9c19e586397f40077f4d6abe2d97b
zDefcon20 <antholozadarequelme@gmail.com>
2024-12-30 13:15:53 -0500

    Update commit

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -1,3 +1,27 @@
 //
 // Created by user on 29/12/2024.
 //
+#include <iostream>
+
+// Funcion para calcular el maximo comun divisor
+
+int mcd(int a, int b) // funcion para mcd
+{
+    if (b == 0)
+    {
+       return a;
+    }
+    else
+    {
+        return mcd(b, a % b);
+    }
+}
+
+int main()
+{
+    int num1, num2;
+    std::cout << "Ingrese el primer numero: "; std::cin >> num1;
+    std::cout << "Ingrese el segundo numero: "; std::cin >> num2;
+
+    std::cout << "El maximo comun divisor de " << num1 << " y " << num2 << " es: " << mcd(num1,num2);
+}
